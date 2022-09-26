b568cc1b253fa64b479ceed6735153c33727b5a1
William Woodruff <william@trailofbits.com>
2022-09-26 17:17:11 -0400

    test: add a test for make_spec
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- test/test_extract.py
+++ test/test_extract.py
@@ -1,6 +1,28 @@
 import pytest
 
-from abi3audit._extract import ExtractorError, WheelExtractor, WheelSpec
+from abi3audit._extract import (
+    ExtractorError,
+    InvalidSpec,
+    PyPISpec,
+    SharedObjectSpec,
+    WheelExtractor,
+    WheelSpec,
+    make_spec,
+)
+
+
+def test_make_spec():
+    assert make_spec("foo.whl") == WheelSpec("foo.whl")
+    assert make_spec("foo.abi3.so") == SharedObjectSpec("foo.abi3.so")
+    assert make_spec("foo") == PyPISpec("foo")
+
+    # Shared objects need to be tagged with `.abi3`.
+    with pytest.raises(InvalidSpec):
+        make_spec("foo.so")
+
+    # Anything that doesn't look like a wheel, shared object, or PyPI package fails.
+    with pytest.raises(InvalidSpec):
+        make_spec("foo@foo")
 
 
 class TestWheelExtractor:
