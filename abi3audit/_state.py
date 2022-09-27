c3a5268576b304305d59cefa641dade47b59fccf
William Woodruff <william@trailofbits.com>
2022-09-27 18:43:51 -0400

    _state: support NO_COLOR
    Closes #11.
    
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- abi3audit/_state.py
+++ abi3audit/_state.py
@@ -2,9 +2,17 @@
 `abi3audit` CLI state, broken out to avoid circular imports.
 """
 
+import os
 import sys
+from typing import Literal, Optional
 
 from rich.console import Console
 
-console = Console(log_path=False, file=sys.stderr)
+_color_system: Optional[Literal["auto"]]
+if os.getenv("NO_COLOR", None) is not None:
+    _color_system = None
+else:
+    _color_system = "auto"
+
+console = Console(log_path=False, file=sys.stderr, color_system=_color_system)
 status = console.status("[green]Processing inputs", spinner="clock")
