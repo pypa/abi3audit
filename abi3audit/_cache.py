81ee88fd4d795cecb0361115d7a7b4a4b0e00f82
William Woodruff <william@trailofbits.com>
2022-10-05 17:12:35 -0400

    pyproject, abi3audit: use requests-cache instead of cachecontrol
    Less configuration, doesn't use multiple open files.
    
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- abi3audit/_cache.py
+++ abi3audit/_cache.py
@@ -2,26 +2,14 @@
 Caching middleware for `abi3audit`.
 """
 
-from pathlib import Path
-
-import requests
-from cachecontrol import CacheControl
-from cachecontrol.caches import FileCache
-
-_ABI3AUDIT_INTERNAL_CACHE = Path.home() / ".cache" / "abi3audit"
+from requests_cache import CachedSession
 
 
-def caching_session() -> CacheControl:
+def caching_session() -> CachedSession:
     """
     Return a `requests` style session, with suitable caching middleware.
     """
 
-    # We limit the number of redirects to 5, since the services we connect to
-    # should really never redirect more than once or twice.
-    inner_session = requests.Session()
-    inner_session.max_redirects = 5
-
-    return CacheControl(
-        inner_session,
-        cache=FileCache(_ABI3AUDIT_INTERNAL_CACHE),
-    )
+    session = CachedSession("abi3audit", use_cache_dir=True)
+    session.max_redirects = 5
+    return session
