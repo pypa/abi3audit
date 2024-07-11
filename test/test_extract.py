1becd5cc5d726a10843bc06a9212561ff0d86a00
William Woodruff <william@trailofbits.com>
2024-07-11 13:58:52 -0400

    cli, extract: support globs
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- test/test_extract.py
+++ test/test_extract.py
@@ -7,22 +7,22 @@ from abi3audit._extract import (
     SharedObjectSpec,
     WheelExtractor,
     WheelSpec,
-    make_spec,
+    make_specs,
 )
 
 
 def test_make_spec():
-    assert make_spec("foo.whl") == WheelSpec("foo.whl")
-    assert make_spec("foo.abi3.so") == SharedObjectSpec("foo.abi3.so")
-    assert make_spec("foo") == PyPISpec("foo")
+    assert make_specs("foo.whl") == [WheelSpec("foo.whl")]
+    assert make_specs("foo.abi3.so") == [SharedObjectSpec("foo.abi3.so")]
+    assert make_specs("foo") == [PyPISpec("foo")]
 
     # Shared objects need to be tagged with `.abi3`.
     with pytest.raises(InvalidSpec):
-        make_spec("foo.so")
+        make_specs("foo.so")
 
     # Anything that doesn't look like a wheel, shared object, or PyPI package fails.
     with pytest.raises(InvalidSpec):
-        make_spec("foo@foo")
+        make_specs("foo@foo")
 
 
 class TestWheelExtractor:
