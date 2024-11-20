f97c38e8a018ddc2956338fc753955739831c338
William S Fulton <wsf@fultondesigns.co.uk>
2024-11-20 21:35:56 +0000

    Fix mypy errors

--- abi3audit/_state.py
+++ abi3audit/_state.py
@@ -6,29 +6,38 @@ from __future__ import annotations
 
 import os
 import sys
-from typing import Literal
+from typing import Any, Literal, Optional, Type
+from types import TracebackType
 
 from rich.console import Console
+from rich.status import Status
 
 
-class Status:
-    def __init__(self, console: Console):
+class StatusWrapper:
+    _status: Status | None
+    def __init__(self, console: Console) -> None:
         self._console = console
         self._status = None
 
-    def __enter__(self):
+    def __enter__(self) -> StatusWrapper:
         if self._status:
             self._status.__enter__()
         return self
 
-    def __exit__(self, *args):
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType],
+    ) -> Any:
         if self._status:
-            self._status.__exit__(*args)
+            return self._status.__exit__(exc_type, exc_val, exc_tb)
+        return False
 
-    def initiate(self):
+    def initiate(self) -> None:
         self._status = self._console.status("[green]Processing inputs", spinner="clock")
 
-    def update(self, s: str):
+    def update(self, s: str) -> None:
         if self._status:
             self._status.update(s)
 
@@ -42,4 +51,4 @@ else:
     _color_system = "auto"
 
 console = Console(log_path=False, file=sys.stderr, color_system=_color_system)
-status = Status(console)
+status = StatusWrapper(console)
